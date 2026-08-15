c4b3ac30c63b12280e1140092b8952618f4019f2
Jorge Castillo <schorts@outlook.com>
2026-08-14 20:33:30 -0600

    feat: Dispatch GetAllInfrastructures Query.

--- app/hooks/all-infrastructures.hook.tsx
+++ app/hooks/all-infrastructures.hook.tsx
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import { useContext, useState, useEffect } from "react";
+
+import { CQRSContext } from "~/contexts";
 
 import type { GetAllInfrastructuresQueryResultDto } from "../../modules/demo/infrastructures/application/dtos";
+import { GetAllInfrastructuresQuery } from "../../modules/demo/infrastructures/application/queries";
 
 export function useAllInfrastructures() {
+  const { queryBus } = useContext(CQRSContext);
   const [loading, setLoading] = useState<boolean>(true);
   const [infrastructures, setInfrastructures] = useState<GetAllInfrastructuresQueryResultDto>([]);
+
+  useEffect(() => {
+    const getAllInfrastructuresQuery = new GetAllInfrastructuresQuery(crypto.randomUUID());
+
+    queryBus
+      .dispatch<GetAllInfrastructuresQuery, GetAllInfrastructuresQueryResultDto>(
+        getAllInfrastructuresQuery,
+      )
+      .then(setInfrastructures)
+      .finally(() => setLoading(false));
+  }, []);
 
   return {
     loading,
